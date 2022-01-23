1592cf2799d6649f40ab0454f7618a22a7609300
Erich <eheiseler@gmail.com>
2022-01-22 21:44:26 -0500

    commit tree 4896956c0846faec03f1c812d6d5d6c8392931a6

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+    //todo 
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    let phrase = string;
+    if(phrase === string.toLowerCase()){
+       return "I can\'t hear you!";
+    }
+    else if(phrase === string.toUpperCase()){
+        return "YES INDEED!";
+    }
+    else if(phrase === "Let\'s have dinner together!"){
+       return "I would love to!";
+    }
+}
